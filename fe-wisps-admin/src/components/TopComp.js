e1588eae0c200aa2cd2093d1e8a925877096b8ca
Wisps <122460516@qq.com>
2025-11-21 18:34:46 +0800

    init

--- fe-wisps-admin/src/components/TopComp.js
+++ fe-wisps-admin/src/components/TopComp.js
@@ -1,24 +1,41 @@
 import { useState } from "react"
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Button, Layout, theme } from 'antd';
-const { token: { colorBgContainer } } = theme.useToken();
+import { MenuFoldOutlined, MenuUnfoldOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Layout, theme, Dropdown, Avatar } from 'antd';
+import { Link } from "react-router-dom";
 const { Header } = Layout;
 
+const items = [
+  {
+    key: '1',
+    label: <Link to='/news'> 个人中心 </Link>,
+    disabled: false,
+    icon: <UserOutlined />
+  },
+  {
+    key: '2',
+    danger: true,
+    label: '退出',
+    icon: <LogoutOutlined />
+  },
+];
+
 const TopComp = ()=>{
+    const { token: { colorBgContainer } } = theme.useToken();
 
     const [collapsed, setCollapsed] = useState(false)
-    
+
     return (
         <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button type="text"
+          <Button type="text" 
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+            onClick={() => setCollapsed(!collapsed)} 
+            style={{ fontSize: '16px', width: 64, height: 64 }} />
+
+          <div style={{float: 'right', marginRight: '20px'}}>
+            <Dropdown menu={{ items }}>
+                <Avatar icon={<UserOutlined />} />
+            </Dropdown>
+          </div>
         </Header>
     )
 }
