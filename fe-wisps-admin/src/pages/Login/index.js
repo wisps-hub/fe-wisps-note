bbaec9407fb04c2f63c9868a90135e8b713fd6d9
huangluping <huangluping@beemwork.com>
2025-11-27 20:13:48 +0800

    login

--- fe-wisps-admin/src/pages/Login/index.js
+++ fe-wisps-admin/src/pages/Login/index.js
@@ -19,55 +19,68 @@ const Login = ()=>{
 
     
     return (
-        <div className='login'>
-            <Button className='qr' style={{border: "none"}}> <QrcodeOutlined /></Button>
-            <div class="bubble">
-                <p>扫码登录</p>
-                <span class="arrow"></span>
+        <div className='container'>
+            <div className='login-sider'>
+                
             </div>
+
             <div className='login-container'>
-                <Card
-                    style={{ width: '100%' , border: 'none'}}
-                    title="欢迎使用BEEM"
-                    tabList={[
-                        {
-                            key: 'phone',
-                            tab: '手机号',
-                        },
-                        {
-                            key: 'email',
-                            tab: '邮箱',
-                        }
-                    ]}
-                    activeTabKey={activeTabKey}
-                    onTabChange={ key => setActiveTabKey(key)}
-                >
-                    <Form >
-                        {inputs[activeTabKey]}
-                        <Form.Item>
-                            <Button size='large' type='primary' htmlType='submit' block>下一步</Button>
-                        </Form.Item>
-                        <Form.Item name="remember" valuePropName="checked" label={null}>
-                            <Checkbox>
-                                <span>
-                                    我已阅读并同意
-                                    <a style={{margin: "0 3px"}} href='http://www.baidu.com'>服务协议</a>
-                                    和
-                                    <a style={{margin: "0 3px"}} href='http://www.baidu.com'>隐私政策</a>
-                                </span>
-                            </Checkbox>
-                        </Form.Item>
-                    </Form>
-                </Card>
-                <div className='otherLogin'>
-                    <Divider className='text'>更多登录方式</Divider>
-                    <Button size="large" icon={<CloudOutlined />} block> SSO 登陆 </Button>
+                <div className='login'>
+
+                    <Button className='qr' style={{border: "none"}}> 
+                        <QrcodeOutlined />
+                    </Button>
+                    <div class="bubble">
+                        <p>扫码登录</p>
+                        <span class="arrow"></span>
+                    </div>
+
+                    <Card
+                        style={{ border: 'none'}}
+                        title="欢迎使用BEEM"
+                        tabList={[
+                            {
+                                key: 'phone',
+                                tab: '手机号',
+                            },
+                            {
+                                key: 'email',
+                                tab: '邮箱',
+                            }
+                        ]}
+                        activeTabKey={activeTabKey}
+                        onTabChange={ key => setActiveTabKey(key)}
+                    >
+                        <Form >
+                            {inputs[activeTabKey]}
+                            <Form.Item>
+                                <Button size='large' type='primary' htmlType='submit' block>下一步</Button>
+                            </Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" label={null}>
+                                <Checkbox>
+                                    <span>
+                                        我已阅读并同意
+                                        <a style={{margin: "0 3px"}} href='http://www.baidu.com'>服务协议</a>
+                                        和
+                                        <a style={{margin: "0 3px"}} href='http://www.baidu.com'>隐私政策</a>
+                                    </span>
+                                </Checkbox>
+                            </Form.Item>
+                        </Form>
+                    </Card>
+
+                    <div className='otherLogin'>
+                        <Divider className='text'>更多登录方式</Divider>
+                        <Button size="large" icon={<CloudOutlined />} block> SSO 登陆 </Button>
+                    </div>
+
+                    <div className='registerText'>
+                        <span>还没有账号？</span>
+                        <a href='http://www.baidu.com'>立即注册</a>
+                    </div>
                 </div>
             </div>
-            <div className='registerText'>
-                <span>还没有账号？</span>
-                <a href='http://www.baidu.com'>立即注册</a>
-            </div>
+
         </div>
             
     )
